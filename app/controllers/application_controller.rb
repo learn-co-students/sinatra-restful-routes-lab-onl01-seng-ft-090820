eb088b64a4ad4307dc2f61c9fab1c20ebbc3a770
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-10-21 10:33:03 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,46 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    #goes to index
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    #displays a form to create a new recipe
+    erb :new
+  end
+
+  post '/recipes' do
+    #creates one article
+    recipe = Recipe.create(params)
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    #displays one recipe based on ID
+    @recipe = Recipe.find_by(params)
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    #displays edit form for a recipe based on the ID
+    @recipe = Recipe.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    #modifies an existing recipe then redirects to that recipe
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    #deletes the article
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.destroy
+  end
 
 end
