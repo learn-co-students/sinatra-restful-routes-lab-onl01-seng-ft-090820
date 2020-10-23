c86dccb898b313f19cdc160c242581281297edaa
doosemavis <moosedavis2011@gmail.com>
2020-10-23 12:13:43 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < Sinatra::Base
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/'
+    erb :index 
+  end
+
+  get '/recipes/new' do 
+    erb :new
+  end 
+  
+  post '/recipes' do 
+    @recipes = Recipe.create(:name => params[:name])
+    redirect to "/recipes/#{@recipe.id}"
+  end
 
 end
