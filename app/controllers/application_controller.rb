ba33f0b4e12c6af2739ffcd96a786ca29f2e16e0
doosemavis <moosedavis2011@gmail.com>
2020-10-23 12:33:18 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/'
-    erb :index 
+    redirect '/recipes' 
   end
+
+  get '/recipes' do 
+    @recipes = Recipe.all 
+    erb :index
+  end 
 
   get '/recipes/new' do 
     erb :new
   end 
   
   post '/recipes' do 
-    @recipes = Recipe.create(:name => params[:name])
-    redirect to "/recipes/#{@recipe.id}"
+    @recipes = Recipe.create(name: params[:name],
+      ingredients: params[:ingredients]
+      cook_time: params[:cook_time]).tap do |recipe|
+        recipe.save
+        redirect to "/recipes/#{@recipe.id}"
+      end
   end
+
+  get 'recipes/:id' do 
+    @recipe = Recipe.find_by(id: params[:id])
+    erb :show
+  end 
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(id: params[:id])
+    erb :edit
+  end 
+
+  patch '/recipes/:id' do
+    Recipe.find_by(id: params[:id]).tap do |recipe|
+      recipe.update(name: params[:name],
+        ingredients: params[:ingredients],
+        cook_time: params[:cook_time])
+    
+      redirect to "/recipes/#{recipe.id}"
+    end
+  end 
+
+  delete '/recipes/:id/delete' do 
+    Recipe.find_by(id: params[:id]).destroy
+    redirect '/recipes'
+  end 
 
 end
--- app/models/recipes.rb
+++ app/models/recipes.rb
@@ -1,4 +1,4 @@
 
 class Recipes < ActiveRecord::Base
-
+    validates_presence_of :name, :ingredients, :cook_time
 end 
